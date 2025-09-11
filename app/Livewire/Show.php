2fa7a163544a959f0800771b6a49046c4cfc74b0
ijsn <ijsn@g2c10.cin.ufpe.br>
2025-09-11 09:27:45 -0300

    add opções de complementos

--- app/Livewire/Show.php
+++ app/Livewire/Show.php
@@ -8,15 +8,45 @@ use Illuminate\Support\Str;
 
 class Show extends Component
 {
- public $id;
- public function mount(){
-  $this->id=request()->route('id');
- }
- public function render(){
-  $produto = Produto::findOrFail($this->id);
-  return view('livewire.show',[
-   'produto'=>$produto,
-   'produtos'=>Produto::all()
-  ]);
- }
+    public $id;
+    public function mount(){
+        $this->id=request()->route('id');
+    }
+ 
+    public function render(){
+        $produtos = Produto::all();
+        $produto = Produto::findOrFail($this->id);
+
+        $frutas = $produtos->filter(function($itens){
+            if(Str::contains($produto->name, ['Açaí Copo','Açaí'])){
+                return Str::contains($itens->descricao,'fruta')
+            }
+        })
+        $vitaminas = $produtos->filter(function($itens){
+            if(Str::contains($produto->name, 'Vitamina de Açaí')){
+                return Str::contains($itens->descricao,'vitamina')
+            }
+        })
+        $caldas = $produtos->filter(function($itens){
+            if(Str::contains($produto->name, ['Açaí Copo','Açaí'])){
+                return Str::contains($itens->descricao,'calda')
+            }
+        })
+        $cremes = $produtos->filter(function($itens){
+            if(Str::contains($produto->name, ['Açaí Copo','Açaí'])){
+                return Str::contains($itens->descricao,'creme')
+            }
+        })
+        $complementos = $produtos->filter(function($itens){
+            if(Str::contains($produto->name, ['Açaí Copo','Açaí'])){
+                return Str::contains($itens->descricao,'complemento')
+            }
+        })
+
+        return view('livewire.show',[
+            'produto'=>$produto,
+            'produtos'=>$produto,
+            'teste'=>$fruta
+        ]);
+    }
 }
